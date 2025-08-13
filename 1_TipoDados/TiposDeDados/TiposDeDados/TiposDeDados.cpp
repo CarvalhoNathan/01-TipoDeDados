1183eeb9e4d001fe27873b0be03861d42705213a
CarvalhoNathan <fariasnathan75@gmail.com>
2025-08-12 23:36:34 -0300

    Atualização segundos

--- 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
+++ 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
@@ -59,6 +59,7 @@ int main()
 	int dias = 0;
 	int horas = 0;
 	int minutos = 0;
+	int segundos = 0;
 
 	cout << "Digite seu nome: ";
 	cin >> nome;
@@ -70,6 +71,7 @@ int main()
 	cout << "Sua idade em dias e: " << dias << endl;
 	cout << "Sua idade em horas e: " << horas << endl;
 	cout << "Sua idade em minutos e: " << minutos << endl;
+	cout << "Sua idade em segundos e: " << minutos * << endl;
 
 	cout << "Qualquer tecla para sair...";
 	system("pause"); // somente no Windows
